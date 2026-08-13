eaaec3b375f0b1e3b376907b18094b3bbd9485b1
UF-FIT <geral@ultimatefit.pt>
2026-08-13 23:36:35 +0100

    Enable clean training presentation enhancer

--- src/lib/trainingExerciseCompletionBootstrap.js
+++ src/lib/trainingExerciseCompletionBootstrap.js
@@ -1,5 +1,7 @@
 import { startTrainingExerciseCompletionEnhancerV2 } from './trainingExerciseCompletionEnhancerV2';
 import { startTrainingLoadAnalytics } from './trainingLoadAnalytics';
+import { startTrainingPresentationEnhancer } from './trainingPresentationEnhancer';
 
 startTrainingExerciseCompletionEnhancerV2();
 startTrainingLoadAnalytics();
+startTrainingPresentationEnhancer();
