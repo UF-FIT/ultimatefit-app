474b0d64064103fc7330511fbcb0e0c66cf60a5e
UF-FIT <geral@ultimatefit.pt>
2026-08-13 23:12:00 +0100

    Add weekly and monthly load analytics

--- src/lib/trainingExerciseCompletionBootstrap.js
+++ src/lib/trainingExerciseCompletionBootstrap.js
@@ -1,3 +1,5 @@
 import { startTrainingExerciseCompletionEnhancerV2 } from './trainingExerciseCompletionEnhancerV2';
+import { startTrainingLoadAnalytics } from './trainingLoadAnalytics';
 
 startTrainingExerciseCompletionEnhancerV2();
+startTrainingLoadAnalytics();
